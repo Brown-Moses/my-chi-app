1ac81d2e1e1058d43face3137f289ffd9a28a0df
Brown-Moses <mosesjustin996@gmail.com>
2025-11-13 01:00:44 +0200

    Refactor student handling: move student logic to services and handlers, implement JSON encoding for student data

--- cmd/main.go
+++ cmd/main.go
@@ -1,24 +1,13 @@
 package main
 
 import (
-	"encoding/json"
+	"chi-app/internal/handlers"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
 )
 
-type Student struct {
-	ID    int    `json:"ID"`
-	Name  string `json:"Name"`
-	Email string `json:"Email"`
-}
-
 // saving some draft students in the list of Student struct... Incase we intergrate DB later> w shall the storage conn instead of a list arr
-var student = []Student{
-	{ID: 1, Name: "Mosesssss", Email: "Moses@gmail.com"},
-	{ID: 2, Name: "Wuruemmmm", Email: "Wuruem@gmail.com"},
-	{ID: 3, Name: "Justinnnn", Email: "Justin@gmail.com"},
-}
 
 func main() {
 	r := chi.NewRouter()
@@ -28,15 +17,7 @@ func main() {
 	})
 
 	// displaying a struct json to browser
-	r.Get("/student", func(w http.ResponseWriter, r *http.Request) {
-
-		//set headers to tell the browser its json
-		w.Header().Set("Content-Type", "Application/json")
-
-		// Encode the struct to JSON and write to response
-		json.NewEncoder(w).Encode(student)
-
-	})
+	r.Get("/student", handlers.GetStudentHandler)
 
 	err := http.ListenAndServe(":8000", r)
 	if err != nil {
